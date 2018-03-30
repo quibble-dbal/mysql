fd4344fbe7647c6f0a8e6ce23ca8b46b0bc7073b
Marijn Ophorst <marijn@sensimedia.nl>
2018-03-30 14:13:28 +0200

    cleanup

--- src/Adapter.php
+++ src/Adapter.php
@@ -5,7 +5,7 @@
  *
  * @package Quibble\Mysql
  * @author Marijn Ophorst <marijn@monomelodies.nl>
- * @copyright MonoMelodies 2008, 2009, 2010, 2011, 2012, 2015, 2016
+ * @copyright MonoMelodies 2008, 2009, 2010, 2011, 2012, 2015, 2016, 2018
  */
 
 namespace Quibble\Mysql;
@@ -15,18 +15,9 @@ use Quibble\Dabble;
 /** MySQL-abstraction class. */
 class Adapter extends Dabble\Adapter
 {
-    public function __construct(
-        $dsn,
-        $username = null,
-        $password = null,
-        array $options = []
-    ) {
-        return parent::__construct(
-            "mysql:$dsn",
-            $username,
-            $password,
-            $options
-        );
+    public function __construct(string $dsn, string $username = null, string $password = null, array $options = [])
+    {
+        return parent::__construct("mysql:$dsn", $username, $password, $options);
     }
 
     public function value($value)
@@ -37,11 +28,17 @@ class Adapter extends Dabble\Adapter
         return parent::value($value);
     }
 
+    /**
+     * @return Quibble\Dabble\Raw
+     */
     public function now() : Dabble\Raw
     {
         return new Dabble\Raw('NOW()');
     }
 
+    /**
+     * @return Quibble\Dabble\Raw
+     */
     public function interval($quantity, $amount) : Dabble\Raw
     {
         $what = null;
@@ -57,6 +54,9 @@ class Adapter extends Dabble\Adapter
         return new Dabble\Raw(sprintf("interval %d %s", $amount, $what));
     }
 
+    /**
+     * @return Quibble\Dabble\Raw
+     */
     public function random() : Dabble\Raw
     {
         return new Dabble\Raw('RAND()');
